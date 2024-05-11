1929df5a2048091564a8babecf5c432eedfd32cd
Muhamad Nur Syami <nursami385@gmail.com>
2024-05-11 13:41:48 +0700

    membuat model

---
+++ app/Models/KomikModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KomikModel extends Model
+{
+    protected $table      = 'komik';
+    // Dates
+    protected $useTimestamps = true;
+}
---
+++ app/Views/komik/index.php
@@ -0,0 +1,37 @@
+<?= $this->extend('layout/template') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mb-3">Daftar Komik</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Sampul</th>
+                        <th scope="col">Judul</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <?php $i = 1 ?>
+                <?php foreach ($komik as $dataKomik) : ?>
+                    <tbody>
+                        <tr>
+                            <th scope="row">
+                                <?= $i++ ?>
+                            </th>
+                            <td><img src="/img/<?= $dataKomik['sampul'] ?>" alt="" class="sampul"></td>
+                            <td><?= $dataKomik['judul'] ?></td>
+                            <td><a href="" class="btn btn-success">Detail</a></td>
+                        </tr>
+
+                    </tbody>
+                <?php endforeach;  ?>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
